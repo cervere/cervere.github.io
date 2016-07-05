afc92619ada64e93a9efcc13e95a4a7d0dc3f501
cervere <bhargav.rrv@gmail.com>
2016-07-05 10:23:38 +0530

    delimitter

--- mypostprocessingscript.php
+++ mypostprocessingscript.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['field1']) && isset($_POST['field2'])) {
-    $data = $_POST['field1'] . '-' . $_POST['field2'] . "\n";
-    $ret = file_put_contents('/tmp/mydata.txt', $data, FILE_APPEND | LOCK_EX);
+    $data = $_POST['field1'] . '|' . $_POST['field2'] . "\n";
+    $ret = file_put_contents('mydata.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
     }

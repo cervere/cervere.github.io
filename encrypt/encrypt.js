bd4e40a003ae7242a6c109b5aa5194a0893f22a9
cervere <bhargav.rrv@gmail.com>
2020-05-09 13:41:47 +0200

    Update encrypt.js

--- encrypt/encrypt.js
+++ encrypt/encrypt.js
@@ -1,41 +1,9 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-/* code by webdevtrick ( https://webdevtrick.com ) */
 var webdevencrypt = {
     setEncrypt: function(source,destination,passcode) {
         document.getElementById(destination).innerText = this.encryptCodes(document.getElementById(source).value,document.getElementById(passcode).value);
     },
     setDecrypt: function() {
-                document.getElementById('decryptedContent').innerText = this.decryptCodes(document.getElementById('originalContent').value,document.getElementById('passcode').value);
+                document.getElementById('decryptedContent').innerText = this.decryptCodes(document.getElementById('thought').value,document.getElementById('passcode').value);
     },
     encryptCodes: function(content,passcode) {
         var result = []; var passLen = passcode.length ;

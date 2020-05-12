0dc09d15ecc84cf18a4b38e9bafe549128504df0
cervere <bhargav.rrv@gmail.com>
2020-05-12 17:20:13 +0200

    Update encrypt.js

--- encrypt/encrypt.js
+++ encrypt/encrypt.js
@@ -29,3 +29,42 @@ var webdevencrypt = {
         return str ;
     }
 }
+
+
+  var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
+  function doCORSRequest(options, printResult) {
+    var x = new XMLHttpRequest();
+    x.open(options.method, cors_api_url + options.url);
+    x.onload = x.onerror = function() {
+      printResult(
+        options.method + ' ' + options.url + '\n' +
+        x.status + ' ' + x.statusText + '\n\n' +
+        (x.responseText || '')
+      );
+    };
+    if (/^POST/i.test(options.method)) {
+      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    }
+    x.send(options.data);
+  }
+
+  // Bind event
+  (function() {
+    var urlField = document.getElementById('category');
+    var dataField = document.getElementById('thought');
+    var outputField = document.getElementById('encryptedOutput');
+    document.getElementById('submit-thought').onclick = function(e) {
+      e.preventDefault();
+      doCORSRequest({
+        method: 'GET',
+        url: 'https://script.google.com/macros/s/AKfycbzZZz38fjoE0B80D_hQiMc14D2c_Ju1sIQlQjGFJAYPcepjVfVx/exec',
+        data: $form.serializeObject()
+      }, function printResult(result) {
+        outputField.value = result;
+      });
+    };
+  })();
+  if (typeof console === 'object') {
+    console.log('// To test a local CORS Anywhere server, set cors_api_url. For example:');
+    console.log('cors_api_url = "http://localhost:8080/"');
+  }

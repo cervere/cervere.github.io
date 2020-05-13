70a6840171627abc2518610218a5b9e842163513
btejanal <bhargav.teja-nallapu@inria.fr>
2020-05-13 13:36:55 +0200

    all changes

--- register/encrypt.js
+++ register/encrypt.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){ 
+    $("#newcategorydiv").hide();
+    $("#categoryform input" ).change(function() {
+    var val = $("input[name=category]:checked", "#categoryform").val();  
+  if(val != undefined) {
+     if (val == "Other") {
+       $("#newcategorydiv").show();
+     } else {
+       $("#newcategorydiv").hide();
+     }
+   }
+   });
+});
 var webdevencrypt = {
     setEncrypt: function(source,destination,passcode) {
         document.getElementById(destination).innerText = this.encryptCodes(document.getElementById(source).value,document.getElementById(passcode).value);
@@ -27,6 +40,32 @@ var webdevencrypt = {
             var ch = String.fromCharCode(result[i]); str += ch ;
         }
         return str ;
+    },
+    getOptions: function(encrypt){
+        var sheets_url = 'https://script.google.com/macros/s/AKfycbzZZz38fjoE0B80D_hQiMc14D2c_Ju1sIQlQjGFJAYPcepjVfVx/exec'        
+        var category = $("input[name=category]:checked", "#categoryform").val();  
+        if (category == "Other") {
+           category = $("#newcategory").val();
+         } 
+        if(category == undefined) {
+          category = 'Miscellaneous'
+        }
+        var thought_or_action = $("input[name=thoughtoraction]:checked", "#thoughtoraction").val();  
+        var subcategory = $("#subcategory").val();
+        var content = $("#content").val();
+        var passcode = document.getElementById('passcode').value;
+        if (passcode == '') {
+          passcode = 'default';
+        }
+        if (encrypt) {
+            content = webdevencrypt.encryptCodes(content,passcode);
+        }
+        options = {
+            method: 'GET',
+            url: sheets_url+'?'+'thought_or_action='+thought_or_action+'&category='+category+'&subcategory='+subcategory+'&content='+content,
+            data: ''
+          }
+        return options
     }
 }
 
